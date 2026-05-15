6d39d4561eaf8b4e2402366d2548dac3dc2ab74a
DaviMartins26 <davi.martins15@outlook.com>
2026-05-15 16:08:10 -0300

    Correcao de Rotas

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -4,19 +4,40 @@ const axios = require('axios');
 
 const URL = process.env.URL_ITENS;
 
-// Rotas que lista os itens
+// Rota que agrega os dois bancos de dados trabalhando com a possivel falha deles
 router.get('/aggregated', async (req, res) => {
+    let itens = [];
+    let usuarios = [];
+
+    // Tenta buscar Itens (Se falhar, itens continua [])
     try {
         const resItens = await axios.get(URL);
+        itens = resItens.data;
+    } catch (e) { console.error("Falha ao buscar itens"); }
+
+    // Tenta buscar Usuários (Se falhar/IP bloqueado, usuarios continua [])
+    try {
         const resUsers = await axios.get(process.env.URL_USUARIOS);
-        
-        res.json({
-            status: "Conecção Estabelecida",
-            total_equipamentos: resItens.data.length,
-            usuarios_ativos: resUsers.data.length,
-            equipamentos: resItens.data
-        });
-    } catch (e) { res.status(500).send("Erro ao agregar dados"); }
+        usuarios = resUsers.data;
+    } catch (e) { console.error("Falha ao buscar usuários, mas o BFF não vai cair!"); }
+
+    // RETORNA UM ÚNICO JSON (Requisito atendido!)
+    res.json({
+        servidor: "LocateHub Ativo",
+        equipamentos: itens,
+        usuariosNoSistema: usuarios,
+        timestamp: new Date()
+    });
+});
+
+// Rota  que busca todos os itens READ (Para ItensList e ItensAdmin)
+router.get('/list', async (req, res) => {
+    try {
+        const response = await axios.get(URL);
+        res.json(response.data); // Retorna só o array de itens do Mongo
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao buscar itens isolados" });
+    }
 });
 
 // Rota que busca item por id READ
@@ -41,6 +62,22 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     await axios.delete(`${URL}/${req.params.id}`);
     res.status(204).send();
+});
+
+// Rota para calcular orçamento via Azure Function
+router.post('/calcular-aluguel', async (req, res) => {
+    try {
+        const { precoDia, dias } = req.body;
+        const urlFunction = process.env.URL_AZURE_FUNCTION;
+
+        // O BFF chama a Azure Function que está rodando na porta 7071
+        const response = await axios.post(urlFunction, { precoDia, dias });
+        
+        res.json(response.data); // Devolve o total calculado para o React
+    } catch (error) {
+        console.error("Erro ao chamar Azure Function:", error.message);
+        res.status(500).json({ error: "Erro no cálculo do orçamento." });
+    }
 });
 
 module.exports = router;
